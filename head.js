1eb5d798d688d1d181037443968a48cad1232a6a
Lionheart250 <jacksonlionheart@gmail.com>
2023-06-26 21:03:54 +0000

    first commit

---
+++ head.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+
+function head(array) {
+  return array[0];
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
