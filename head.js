dec8c2685826bb5333ada639facaa12cfd4dec4b
Lionheart250 <jacksonlionheart@gmail.com>
2023-06-27 15:03:24 +0000

    hi

--- head.js
+++ head.js
@@ -2,6 +2,8 @@ const assertEqual = function(actual, expected) {
 
 function head(array) {
   return array[0];
-
+}
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+console.log(head)
